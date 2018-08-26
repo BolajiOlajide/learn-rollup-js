1ff6d57c8f9389c7811324732dc97c2fa064d98e
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-26 12:28:18 +0100

    configure eslint with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import babel from 'rollup-plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
 
 export default {
-  entry: 'src/scripts/main.js',
-  dest: 'build/js/main.min.js',
-  format: 'iife',
-  sourceMap: 'inline',
+  input: 'src/scripts/main.js',
+  output: {
+    file: 'build/js/main.min.js',
+    format: 'iife',
+    sourcemap: 'inline',
+  },
   plugins: [
+    eslint({
+      exclude: [
+        'src/styles/**'
+      ]
+    }),
     babel({
       exclude: 'node_modules/**'
     })
